aa87f2b057166303135408705e7bb6d2122b2992
Rajat <rbudania99@gmail.com>
2020-04-14 01:57:04 +0530

    Updates

---
+++ precache-manifest.faf37bd48fdc10dc00013f4c9059021e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2198fb1595e1c72511037f681db0863e",
+    "url": "/college-login/index.html"
+  },
+  {
+    "revision": "da09c20063347c9bee2f",
+    "url": "/college-login/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "798518bee6c003cf3bc4",
+    "url": "/college-login/static/css/main.2f6626f0.chunk.css"
+  },
+  {
+    "revision": "da09c20063347c9bee2f",
+    "url": "/college-login/static/js/2.6f9ff3ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/college-login/static/js/2.6f9ff3ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "798518bee6c003cf3bc4",
+    "url": "/college-login/static/js/main.0da1dd56.chunk.js"
+  },
+  {
+    "revision": "43d65b3d051eb4e2243a",
+    "url": "/college-login/static/js/runtime-main.492ad7c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/college-login/static/media/logo.5d5d9eef.svg"
+  }
+]);
